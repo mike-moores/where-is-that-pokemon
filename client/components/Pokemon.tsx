efe175921901e7a4039df14611f1c02930e54ae5
Sandra Laban <sandra.muncan@gmail.com>
2023-11-17 15:09:04 +1300

    shuffle works

--- client/components/Pokemon.tsx
+++ client/components/Pokemon.tsx
@@ -1,7 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
-import { useState, useEffect } from 'react'
-import { fetchPokemmon } from '../apiClient'
+import { useState, useEffect, useMemo } from 'react'
+import { fetchPokemon } from '../apiClient'
 import type { PokemonData } from '../models/pokemon'
+
+function shuffleArray<T>(inputArray: Array<T>): Array<T> {
+  const array = [...inputArray]
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
 
 interface selctedCard {
   id: number
@@ -15,12 +24,13 @@ export default function Pokemon() {
     isLoading,
   } = useQuery({
     queryKey: ['pokemon'],
-    queryFn: fetchPokemmon,
+    queryFn: fetchPokemon,
   })
 
   const [revealedCards, setRevealedCards] = useState<number[]>([])
   const [choiceOne, setChoiceOne] = useState<selctedCard | null>(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [revealedDeck, setRevealedDeck] = useState(null as null | string[])
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
@@ -32,7 +42,23 @@ export default function Pokemon() {
     }
   }, [choiceOne, choiceTwo])
 
+  useEffect(() => {
+    if (pokemonData != undefined) {
+      const cards = shuffleArray(pokemonData.data)
+        .slice(0, 9)
+        .map((_) => _.images.small)
+      const cards2 = shuffleArray([...cards, ...cards])
+      setRevealedDeck(cards2)
+    } else {
+      setRevealedDeck(null)
+    }
+  }, [pokemonData])
+
   const handleCardClick = (index: number) => {
+    if (revealedDeck == undefined) {
+      return
+    }
+
     // Toggle the card's visibility by adding or removing it from the revealedCards array
     console.log(revealedCards.length + ',' + revealedDeck.length)
     if (choiceOne == null) {
@@ -49,7 +75,7 @@ export default function Pokemon() {
         setTimeout(() => {
           flipCard(index)
           flipCard(choiceOne.id)
-        }, 3000)
+        }, 1000)
         setChoiceOne(null)
       }
     }
@@ -70,7 +96,7 @@ export default function Pokemon() {
     return <p>There was an error</p>
   }
 
-  if (isLoading) {
+  if (isLoading || pokemonData == undefined || revealedDeck == undefined) {
     return <p>Pokemon are loading</p>
   }
 
@@ -95,30 +121,12 @@ export default function Pokemon() {
     './client/images/pokemon-card.png',
   ]
 
-  const revealedDeck = [
-    pokemonData.data[0].images.small,
-    pokemonData.data[1].images.small,
-    pokemonData.data[2].images.small,
-    pokemonData.data[4].images.small,
-    pokemonData.data[5].images.small,
-    pokemonData.data[6].images.small,
-    pokemonData.data[7].images.small,
-    pokemonData.data[8].images.small,
-    pokemonData.data[9].images.small,
-    pokemonData.data[0].images.small,
-    pokemonData.data[1].images.small,
-    pokemonData.data[2].images.small,
-    pokemonData.data[4].images.small,
-    pokemonData.data[5].images.small,
-    pokemonData.data[6].images.small,
-    pokemonData.data[7].images.small,
-    pokemonData.data[8].images.small,
-    pokemonData.data[9].images.small,
-  ]
+  // Shuffle the revealedDeck array
 
   return (
     <div>
       <h2>Pokemon list</h2>
+      {/* <button onClick={shuffleArray}>New Game</button> */}
       <div className="deck">
         {initialDeck.map((card, index) => (
           <div
